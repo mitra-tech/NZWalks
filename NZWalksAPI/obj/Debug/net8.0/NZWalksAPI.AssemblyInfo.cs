699917fcf6831d52dbe23941a077a1877ab9f893
mitra-tech <davoudabadi.mitra@gmail.com>
2024-01-23 22:39:59 -0700

    adds DI

--- NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
+++ NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613fe3b498b38163068477ea7cadf141b5ddcb85")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
