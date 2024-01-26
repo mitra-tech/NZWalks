30fa3ccce9ea4a007235eaf32511c04475357e23
mitra-tech <davoudabadi.mitra@gmail.com>
2024-01-26 11:09:53 -0700

    adds dbcontext to the controller

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domains;
+
+namespace NZWalksAPI.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // GET SINGLE REGION (Get Refion By ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) 
+        {
+            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+            if (region == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(region);
+        }
+    }
+}
--- NZWalksAPI/Program.cs
+++ NZWalksAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
